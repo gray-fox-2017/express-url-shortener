9a1cd4aa788ee0cacb6c43791fa02475d4e223ce
ambodalle <ambodalle.st@gmail.com>
2017-05-16 13:15:38 +0700

    shortener url done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  models.Url.findAll()
+  .then(data =>{
+    res.render('index', {
+      data : data
+    })
+  })
 });
+
+router.post('/urls' ,function(req,res,next) {
+  models.Url.create({
+    url : req.body.url,
+    shortener_url : req.body.shortener_url
+  })
+  .then(function(user) {
+    res.redirect('/')
+  })
+})
+
+router.get('/:short_url', function(req,res,next) {
+  model.Url.find({
+    where : {
+      shortener_url : req.params.shortener_url
+    }
+  })
+  .then ((url) =>{
+    let last = url.count;
+    url.updateAttributes({
+      count : last+1
+    })
+    .then(()=>{
+      res.redirect('/')
+    })
+  })
+})
+
 
 module.exports = router;
