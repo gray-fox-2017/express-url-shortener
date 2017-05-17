54afbe6cb29bc44f258db1c475f6ef6c38e448db
fajarkarim <fajarabdulkarim@gmail.com>
2017-05-17 16:59:27 +0700

    change to deploy mode

--- models/url.js
+++ models/url.js
@@ -15,15 +15,15 @@ module.exports = function(sequelize, DataTypes) {
   },{
     hooks: {
       beforeCreate : function(Urls) {
-          let deployUrl = "";
-          let devUrl = "localhost:3000/";
+          let deployUrl = "https://pendek-url.herokuapp.com/";
+          let devUrl = "http://localhost:3000/";
           let length = 5
           let chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
           let result = '';
           for (var i = length; i > 0; --i) {
             result += chars[Math.floor(Math.random() * chars.length)]
           };
-          Urls.short_url = devUrl + result;
+          Urls.short_url = deployUrl + result;
 
       }
     }
